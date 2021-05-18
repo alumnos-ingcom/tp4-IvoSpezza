326a7a3cc97c036ec6e8f2c5bc2688392630beba
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-18 19:33:55 -0300

    comenzando ejercicio 3

---
+++ tp4-ej3.py
@@ -0,0 +1,21 @@
+################
+# Ivo Spezzacatena - @IvoSpezza
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+def convertir_a_fahrrenheit(centigrados):
+def convertir_a_centigrados(fahrenheit):
+    
+    
+    
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()
+    convertir_a_fahrrenheit("")
+    convertir_a_centigrados("")
