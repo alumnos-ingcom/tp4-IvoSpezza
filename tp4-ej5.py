ea41757e810aae220eb3bb9527d9b03013b00e69
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-18 20:21:55 -0300

    ejercicio 5

---
+++ tp4-ej5.py
@@ -0,0 +1,31 @@
+################
+# Ivo Spezzacatena - @IvoSpezza
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+def signo(numero):
+    numerosiosi = True
+    while numerosiosi:
+        try:
+            numero = int(input("Introduzca un valor negativo o positivo: "))
+            numerosiosi = False
+        except ValueError:
+            print("valor no utilizable, reintente")
+    if numero > 0:
+        print(f"{numero} es positivo")
+    elif numero < 0:
+        print(f"{numero} es negativo")
+    else:
+        print("usted introdujo 0")
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()
+    signo("")
+    
