9a2e7742f447e545215b5dab54b28ebc2dd00e46
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-18 19:51:54 -0300

    ejercicio 3 completado

--- tp4-ej3.py
+++ tp4-ej3.py
@@ -8,8 +8,32 @@
 # Reemplazar por las funciones del ejercicio
 
 def convertir_a_fahrrenheit(centigrados):
-def convertir_a_centigrados(fahrenheit):
+    validante = True
     
+    while validante:
+        try:
+            centigrados = float(input("ingrese la t° ambiente en centigrados "))
+            validante = False
+        except ValueError:
+            print(f"{centigrados} tiene que ser un numero, reintente")
+    
+    fahrenheit = (centigrados * 1.8) + 32
+    
+    print(f"{centigrados}° centigrados son {fahrenheit}° grados fahrenheit")
+    
+def convertir_a_centigrados(fahrenheit):
+    validante = True
+    
+    while validante:
+        try:
+            fahrenheit = float(input("ingrese la t° ambiente en fahrenheit "))
+            validante = False
+        except ValueError:
+            print(f"{fahrenheit} tiene que ser un numero, reintente")
+    
+    centigrados = (fahrenheit -32) / 1.8
+    
+    print(f"{fahrenheit}° grados fahrenheit son {centigrados}° centigrados ")
     
     
 def prueba():
