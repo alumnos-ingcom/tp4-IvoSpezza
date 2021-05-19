96035cb3062a7a71885a47c210f29f74b40d8dd4
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-19 20:17:26 -0300

    ejercicio 8

--- plantilla.py
+++ plantilla.py
@@ -7,9 +7,60 @@
 
 # Reemplazar por las funciones del ejercicio
 
-def prueba():
-    pass
+def ordenar_mayor_a_menor(uno, dos, tres):
+    
+    validador_de_n = True
+    
+    print("tupla de mayor a menor")
+    
+    while validador_de_n:
+        try:
+            uno = int(input("valor a "))
+            dos = int(input("valor b "))
+            tres = int(input("valor c "))
+            validador_de_n= False
+        except ValueError:
+            print("tiene que ser un numero entero")
+            
+    lista =  [uno, dos , tres]
+    
+    lista.sort()
+    lista.reverse()
+    
+    la_tupla = tuple(lista)
+    
+    return la_tupla
+    
+    
+def ordenar_menor_a_mayor(uno, dos, tres):
+    validador_de_n = True
+    
+    print("tupla de menor a mayor")
+    
+    while validador_de_n:
+        try:
+            uno = int(input("valor a "))
+            dos = int(input("valor b "))
+            tres = int(input("valor c "))
+            validador_de_n= False
+        except ValueError:
+            print("tiene que ser un numero entero")
+            
+    lista =  [uno, dos , tres]
+    
+    lista.sort()
+    
+    la_tupla = tuple(lista)
+    
+    return la_tupla
 
+    
+def prueba():
+    
+    ordenar_mayor_a_menor("","","")
+    
+    ordenar_menor_a_mayor("","","")
+    
 if __name__ == "__main__":
     prueba()
     
---
+++ tp4-ej8.py
@@ -0,0 +1,16 @@
+################
+# Ivo Spezzacatena - @IvoSpezza
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()
+    
+    
