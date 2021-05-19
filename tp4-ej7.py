fb8f367b6aec589cddcf173762ee009d3c76f3ba
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-19 18:59:05 -0300

    ejercicio 7
    me gustaría saber si ahora están bien los ejercicios

--- tp4-ej7.py
+++ tp4-ej7.py
@@ -8,9 +8,45 @@
 # Reemplazar por las funciones del ejercicio
 
 
+def division_lenta(dividendo, divisor):
+    validante = True
+    while validante:
+        try:
+            dividendo = int(input("ingrese valor a dividir "))
+            divisor = int(input("ingrese divisor "))
+            validante = False
+        except ValueError:
+            print("el valor ingresado debe ser un numero")
+    
+    cociente = 0
+    if dividendo > 0 and divisor < 0 or dividendo < 0 and divisor > 0:
+        
+        resto = abs(dividendo)
+        divisor = abs(divisor)
+        
+        while resto > 0:
+            resto -= divisor
+            cociente += 1
+            
+        cociente = -1 * cociente
+        print(f"al dividir '{dividendo}' por '{divisor}', obtenemos como cociente '{cociente}' y como resto '{resto}'")
+        
+    elif divisor == 0 or dividendo == 0:
+        
+        print("ERROR FATAL, error al dividir por 0")
+        
+    elif dividendo > 0 and divisor > 0:
+        
+        resto = dividendo
+        while resto > 0:
+            resto -= divisor
+            cociente += 1
+        print(f"al dividir '{dividendo}' por '{divisor}', obtenemos como cociente '{cociente}' y como resto '{resto}'")
+
 
 def prueba():
-    pass
+    
+    division_lenta("","")   
 
 if __name__ == "__main__":
     prueba()
