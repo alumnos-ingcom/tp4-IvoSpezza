6d48a127788a8c61342535c6c98ff62b78d40f4f
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-20 16:19:26 -0300

    @martinvilu esta bien asi???

--- tp4-ej2.py
+++ tp4-ej2.py
@@ -6,19 +6,10 @@
 
 
 # Reemplazar por las funciones del ejercicio
+from tp4ej1 import ingreso_entero_reintento
+
 
 def suma_lenta(numero, otro_numero):
-    
-    validador_de_n = True
-    
-    while validador_de_n:
-        try:
-            numero = int(input("valor a "))
-            otro_numero = int(input("valor b "))
-            validador_de_n= False
-        except ValueError:
-            print("tiene que ser un numero entero")
-    print(f"Procesando {numero} + {otro_numero} ...")
     
     resultado = numero + otro_numero
     suma_lenta = 0
@@ -33,12 +24,11 @@ def suma_lenta(numero, otro_numero):
         print(f"{suma_lenta}")
         suma_lenta += 1
     print(f"{suma_lenta}")
-    print("proceso terminado.")
     
 def prueba():
     
-    suma_lenta("", "")
-
+    suma_lenta(ingreso_entero_reintento("ingrese valor a, "), ingreso_entero_reintento("ingrese valor b, ")) 
+    
 if __name__ == "__main__":
     prueba()
     
