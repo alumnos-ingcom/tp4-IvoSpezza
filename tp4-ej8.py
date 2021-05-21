a765305e6ed82fcec264a4965a1d91ffa479ee91
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-21 02:01:24 -0300

    corregidos 2 errores
    el asistente me generaba problemas por espacios. ejercicio 8 adaptado a lo que entendí

--- tp4-ej8.py
+++ tp4-ej8.py
@@ -7,59 +7,33 @@
 
 # Reemplazar por las funciones del ejercicio
 
+from tp4ej1 import ingreso_entero_reintento
+
+
+
 def ordenar_mayor_a_menor(uno, dos, tres):
-    
-    validador_de_n = True
-    
-    print("tupla de mayor a menor")
-    
-    while validador_de_n:
-        try:
-            uno = int(input("valor a "))
-            dos = int(input("valor b "))
-            tres = int(input("valor c "))
-            validador_de_n= False
-        except ValueError:
-            print("tiene que ser un numero entero")
             
-    lista =  [uno, dos , tres]
-    
+    lista = [uno, dos , tres]
     lista.sort()
     lista.reverse()
-    
-    la_tupla = tuple(lista)
-    
-    return la_tupla
-    
+    la_tupla = tuple(lista) 
+    return la_tupla    
     
 def ordenar_menor_a_mayor(uno, dos, tres):
-    validador_de_n = True
-    
-    print("tupla de menor a mayor")
-    
-    while validador_de_n:
-        try:
-            uno = int(input("valor a "))
-            dos = int(input("valor b "))
-            tres = int(input("valor c "))
-            validador_de_n= False
-        except ValueError:
-            print("tiene que ser un numero entero")
             
     lista =  [uno, dos , tres]
-    
-    lista.sort()
-    
-    la_tupla = tuple(lista)
-    
+    lista.sort() 
+    la_tupla = tuple(lista)   
     return la_tupla
 
-    
 def prueba():
     
-    ordenar_mayor_a_menor("","","")
-    
-    ordenar_menor_a_mayor("","","")
+    valorA = ingreso_entero_reintento("ingrese valor A,")
+    valorB = ingreso_entero_reintento("ingrese valor B,")
+    valorC = ingreso_entero_reintento("ingrese valor C,")   
+    ordenadoMm = ordenar_mayor_a_menor(valorA, valorB, valorC)
+    ordenadomM = ordenar_menor_a_mayor(valorA, valorB, valorC)
+    print(f"la tuple: '{valorA}, {valorB}, {valorC}' ordenada de mayor a menor = {ordenadoMm} , de menor a mayor = {ordenadomM}")
     
 if __name__ == "__main__":
     prueba()
--- tp4ej1.py
+++ tp4ej1.py
@@ -11,46 +11,27 @@
 
 def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
    
-    while cantidad_reintentos != 0:
-        
-        numero = input(f"{mensaje} tienes {cantidad_reintentos} intentos ")
-        
+    while cantidad_reintentos != 0:    
+        numero = input(f"{mensaje} tienes {cantidad_reintentos} intentos ")   
         try:
             numero_entero = int(numero)
             return numero_entero
-            break
-        
-        except ValueError as err:
-            
-            cantidad_reintentos -= 1
-            
-            if cantidad_reintentos > 0:
-                
-                print(f"te queda {cantidad_reintentos} intentos, no falles!!!")
-                
-            else:
-                
+          
+        except ValueError as err:       
+            cantidad_reintentos -= 1       
+            if cantidad_reintentos > 0:          
+                print(f"te queda {cantidad_reintentos} intentos, no falles!!!")              
+            else:             
                 raise IngresoIncorrecto("fallaste 5 veces seguidas... increible") from err
-
-
-
-
-def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
-                
-        valor = ingreso_entero_reintento(f"{mensaje},")  
-
-        if valor > valor_minimo and valor < valor_maximo:
-            print(f"0 < {valor} < 10")
-
-                
-        elif valor < valor_minimo:
-            raise IngresoIncorrecto(f"el numero tiene que ser mayor a {valor_minimo}")
-                
-        elif valor > valor_maximo:
-            raise IngresoIncorrecto(f"el numero tiene que ser menor a {valor_maximo}")
-                
-        
             
+def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):                
+    valor = ingreso_entero_reintento(f"{mensaje},")  
+    if valor > valor_minimo and valor < valor_maximo:
+        print(f"0 < {valor} < 10")               
+    elif valor < valor_minimo:
+        raise IngresoIncorrecto(f"el numero tiene que ser mayor a {valor_minimo}")           
+    elif valor > valor_maximo:
+        raise IngresoIncorrecto(f"el numero tiene que ser menor a {valor_maximo}")
         
 class IngresoIncorrecto(Exception):
     pass
@@ -58,8 +39,7 @@ class IngresoIncorrecto(Exception):
 def prueba():
     
     ingreso_entero_restringido("ingrese valor entre 0 y 10 ")
-    
-    
+   
 if __name__ == "__main__":
     prueba()
     
