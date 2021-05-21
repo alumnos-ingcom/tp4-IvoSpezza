1118f3fdc3cb6cd6df2a352bc76eeb752dd1fbd3
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-05-21 01:17:06 -0300

    ej6, ya es como la 3ra corrección de todos los ej, espero sea asi

--- tp4-ej6.py
+++ tp4-ej6.py
@@ -7,10 +7,14 @@
 
 # Reemplazar por las funciones del ejercicio
 
-def minimo(lista):
+
+
+def creador_de_listas():
+
     lista = []
     error = ("el valor ingresado debia ser un numero entero!")
     validante_de_enteros = True
+    
     while validante_de_enteros:
         try:
             cantidad = int(input("¿Cuantos valores quiere en su lista? "))
@@ -27,14 +31,19 @@ def minimo(lista):
     variable_para_el_mensaje = cantidad
     
     validante_de_enteros = True
+    
     while validante_de_enteros:
         try:
             while cantidad > 0:
                 lista.append(int(input(f"ingrese el valor entero n°{variable_para_el_mensaje - cantidad +1 } : ")))
                 cantidad -= 1
             validante_de_enteros = False
+            return lista
         except ValueError:
             print(error)
+
+def minimo(lista):
+    
     print(f"esta es tu lista: {lista}")
     lista.sort()
     print(f"el menor numero en la lista indicada es: {lista[0]}")
@@ -42,33 +51,7 @@ def minimo(lista):
     
     
 def maximo(lista):
-    lista = []
-    error = ("el valor ingresado debia ser un numero entero!")
-    validante_de_enteros = True
-    while validante_de_enteros:
-        try:
-            cantidad = int(input("¿Cuantos valores quiere en su lista? "))
-            if cantidad < 0:
-                cantidad = cantidad * -1
-                print(f"no se aceptan numero negativos, el valor {cantidad*-1} ahora es {cantidad}")
-            elif cantidad == 0:
-                cantidad += 1
-                print("0 no es aceptado, tu lista tendra 1 solo valor")
-            validante_de_enteros = False
-        except ValueError:
-            print(error)
     
-    variable_para_el_mensaje = cantidad
-    
-    validante_de_enteros = True
-    while validante_de_enteros:
-        try:
-            while cantidad > 0:
-                lista.append(int(input(f"ingrese el valor entero n°{variable_para_el_mensaje - cantidad +1 } : ")))
-                cantidad -= 1
-            validante_de_enteros = False
-        except ValueError:
-            print(error)
     print(f"esta es tu lista: {lista}")
     lista.sort()
     lista.reverse()
@@ -77,8 +60,10 @@ def maximo(lista):
 
 def prueba():
     
-    minimo("")
-    maximo("")
+    lista_creada = creador_de_listas()
+    
+    minimo(lista_creada)
+    maximo(lista_creada)
     
 if __name__ == "__main__":
     prueba()
